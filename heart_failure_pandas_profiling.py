252b9995256fbd77ec820a7cd54a81e9ab11a93c
regan <reganreinal@gmai.com>
2020-12-08 19:35:34 +0700

    Fixed result
    now can save picture and then we can show it later

--- heart_failure_pandas_profiling.py
+++ heart_failure_pandas_profiling.py
@@ -1,8 +1,8 @@
 #!C:/Users/Asus/AppData/Local/Programs/Python/Python38/python.exe
 import numpy as np
 import pandas as pd
-
-pip install https://github.com/pandas-profiling/pandas-profiling/archive/master.zip
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 import pandas_profiling as pdp
 
@@ -13,8 +13,7 @@ df = pd.read_csv(input_)
 
 report = pdp.ProfileReport(df, title='Pandas Profiling Report')
 
-report.to_widgets()
-
 plt.figure(figsize=(15,15))
 sns.heatmap(df.corr(),annot=True)
 plt.show()
+plt.savefig('pearson_correlation.png')
