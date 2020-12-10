abcfbc80f7bf37d8b2adf8fecec2fc0101c9e07f
Crucios <57003452+Crucios@users.noreply.github.com>
2020-12-10 18:43:34 +0700

    Update home.php

--- home.php
+++ home.php
@@ -42,7 +42,7 @@ require_once 'dbconnect.php';
                 }
                 ?>
             </h3>
-            <img id="correlation" src="pearson_correlation.png"></img>
+            <img id="correlation" src="pearson_correlation.png" style="width: 90%; height: auto;"></img>
         </div>
         <br>
         <h2>Facts About Heart Failures</h2><br>
